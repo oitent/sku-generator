af0e1cd21211c3540f2df8fd6a45e161136a5041
Andre Renault <27972873+oitent@users.noreply.github.com>
2023-05-08 18:30:44 -0300

    Add better styling

--- pages/index.js
+++ pages/index.js
@@ -53,53 +53,57 @@ const IndexPage = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">SKU Generator</h1>
-      <form onSubmit={handleSubmit} className="space-y-4 flex flex-col w-full">
-        <ul className='flex flex-row gap-4'>
-          {Object.keys(parametersData).map((parameter) => (
-            <li key={parameter} className='flex flex-col'>
-              <p className="text-lg font-semibold mb-2">{parameter}:</p>
-              {parametersData[parameter].map((option) => (
-                <label
-                  key={option.id}
-                  className="inline-flex items-center space-x-1 mr-4 mb-2"
-                >
-                  <input
-                    type="checkbox"
-                    value={option.id}
-                    onChange={(e) => handleChange(e, parameter, option.id)}
-                    className="rounded-md border-gray-300"
-                  />
-                  <span>{option.value}</span>
-                </label>
-              ))}
-            </li>
-          ))}</ul>
-        <div>
-          <button
-            type="submit"
-            className="px-4 py-2 bg-blue-600 text-white rounded-md mt-4"
-          >
-            Generate SKUs
-          </button></div>
-      </form>
-      <h2 className="text-2xl font-bold mt-8 mb-4">Generated SKUs</h2>
-      <table className="w-full table-auto">
-        <thead>
-          <tr>
-            <th className="border border-gray-300 px-4 py-2">SKU</th>
-          </tr>
-        </thead>
-        <tbody>
-          {skuList.map((sku, index) => (
-            <tr key={index} className="bg-gray-100">
-              <td className="border border-gray-300 px-4 py-2">{sku}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <main className='dark:bg-black bg-gray-100 min-h-screen h-full'>
+      <div className="mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-4 dark:text-white text-black">SKU Generator</h1>
+        <form onSubmit={handleSubmit} className="flex flex-row w-full justify-between items-start gap-4">
+          <ul className='flex flex-row gap-4 bg-gray-200 dark:bg-gray-800 rounded-lg p-6'>
+
+            {Object.keys(parametersData).map((parameter) => (
+
+              <li key={parameter} className='flex flex-col' data-tooltip-target="tooltip-default">
+                <p className="text-lg font-semibold mb-2 text-black dark:text-gray-200">{parameter}:</p>
+                {parametersData[parameter].map((option) => (
+                  <label
+                    key={option.id}
+                    className="inline-flex items-center space-x-1 mr-4 mb-2 text-black dark:text-gray-400"
+                  >
+                    <input
+                      type="checkbox"
+                      value={option.id}
+                      onChange={(e) => handleChange(e, parameter, option.id)}
+                      className="rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-300 mr-1"
+                    />
+                    <span>{option.value}</span>
+                  </label>
+                ))}
+              </li>
+
+            ))}
+
+          </ul>
+          <div className='flex flex-col w-auto flex-grow flex-wrap bg-gray-100 dark:bg-gray-900 p-6 rounded-lg border border-gray-200 dark:border-gray-500'>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-600 text-white rounded-md"
+            >
+              Generate SKUs
+            </button>
+
+            <h2 className="text-2xl font-bold mt-8 mb-4 text-black dark:text-white">Generated SKUs</h2>
+            <table className="w-full table-auto">
+              <tbody>
+                {skuList.map((sku, index) => (
+                  <tr key={index} className="bg-gray-700">
+                    <td className="px-4 leading-tallest text-white font-mono">{sku}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,112 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss").Config} */
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./lib/components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{jpg,png,svg,webp}",
+  ],
+  plugins: [
+    require("flowbite/plugin"),
+  ],
+  darkMode: "media",
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        gray: colors.stone,
+        info: colors.teal,
+        secondary: colors.amber,
+        mermaid: { "50": "#B50000", "100": "#8A0000", "200": "#d10000", "300": "#d10000", "400": "#d10000", "500": "#F10101" }
+      },
+      dropShadow: {
+        "special": [
+          "0 2.8px 2.2px rgba(0, 0, 0, 0.034)",
+          "0 6.7px 5.3px rgba(0, 0, 0, 0.048)",
+          "0 12.5px 10px rgba(0, 0, 0, 0.06)",
+          "0 22.3px 17.9px rgba(0, 0, 0, 0.072)",
+          "0 41.8px 33.4px rgba(0, 0, 0, 0.086)",
+          "0 100px 80px rgba(0, 0, 0, 0.12)"
+        ]
+      },
+      fontSize: {
+        xxxs: "0.4rem",
+        xxs: "0.6rem",
+        xs: "0.8rem",
+        s: "0.9rem",
+        x: "1rem",
+        xxl: "2rem",
+        xxxl: "3rem",
+        xxxxl: "5rem"
+      },
+      lineHeight: {
+        shortest: ".6rem",
+        shorter: ".8rem",
+        normal: "1rem",
+        xxxs: "1rem",
+        short: "1.2rem",
+        tall: "1.4rem",
+        taller: "1.5rem",
+        tallest: "2rem",
+        xxs: "2.8rem",
+        xs: "3.9rem",
+        x: "4.5rem",
+        xl: "6rem",
+        xxl: "8rem",
+        xxxl: "10rem",
+        nil: "0rem",
+        none: "1",
+      },
+      letterSpacing: {
+        tightest: "-.075em",
+        tighter: "-.05em",
+        tight: "-.03em",
+        normal: "0",
+        wide: ".03em",
+        wider: ".05em",
+        widest: ".09em",
+        wild: ".25em",
+      },
+    },
+    fontFamily: {
+      "body": [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji"
+      ],
+      "sans": [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji"
+      ]
+    }
+
+  }
 }
