2b4b42b3fe658e8c4607a4efb74a4ad28760beba
Andre Renault <27972873+oitent@users.noreply.github.com>
2023-05-09 12:36:16 -0300

    Add sorting options and search input

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ const generateSKUCombinations = (selectedParameters) => {
 const IndexPage = () => {
   const [selectedParameters, setSelectedParameters] = useState({});
   const [skuList, setSkuList] = useState([]);
+  const [searchTerms, setSearchTerms] = useState({});
 
   const handleChange = (e, parameter, value) => {
     const isChecked = e.target.checked;
@@ -46,6 +47,27 @@ const IndexPage = () => {
     }
   };
 
+  const handleSearch = (e, parameter) => {
+    setSearchTerms({
+      ...searchTerms,
+      [parameter]: e.target.value,
+    });
+  };
+
+  const filteredOptions = (parameter) => {
+    const searchTerm = searchTerms[parameter]?.toLowerCase() || '';
+    return parametersData[parameter]
+      .filter((option) =>
+        option.value.toLowerCase().includes(searchTerm)
+      )
+      .sort((a, b) => {
+        if (a.value === 'NÃO TEM') return -1;
+        if (b.value === 'NÃO TEM') return 1;
+        return a.value.localeCompare(b.value);
+      });
+  };
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const combinations = generateSKUCombinations(selectedParameters);
@@ -57,13 +79,25 @@ const IndexPage = () => {
       <div className="mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold mb-4 dark:text-white text-black">SKU Generator</h1>
         <form onSubmit={handleSubmit} className="flex flex-row w-full justify-between items-start gap-4">
-          <ul className='flex flex-row gap-4 bg-gray-200 dark:bg-gray-800 rounded-lg p-6'>
 
+          <ul className="flex flex-row gap-4 bg-gray-200 dark:bg-gray-800 rounded-lg p-6">
             {Object.keys(parametersData).map((parameter) => (
-
-              <li key={parameter} className='flex flex-col' data-tooltip-target="tooltip-default">
-                <p className="text-lg font-semibold mb-2 text-black dark:text-gray-200">{parameter}:</p>
-                {parametersData[parameter].map((option) => (
+              <li
+                key={parameter}
+                className="flex flex-col"
+                data-tooltip-target="tooltip-default"
+              >
+                <p className="text-lg font-semibold mb-2 text-black dark:text-gray-200">
+                  {parameter}:
+                </p>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  value={searchTerms[parameter] || ''}
+                  onChange={(e) => handleSearch(e, parameter)}
+                  className="mb-2 p-1 border border-gray-300 dark:border-gray-700 dark:bg-gray-800 rounded-md"
+                />
+                {filteredOptions(parameter).map((option) => (
                   <label
                     key={option.id}
                     className="inline-flex items-center space-x-1 mr-4 mb-2 text-black dark:text-gray-400"
@@ -72,25 +106,25 @@ const IndexPage = () => {
                       type="checkbox"
                       value={option.id}
                       onChange={(e) => handleChange(e, parameter, option.id)}
+                      required
                       className="rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-300 mr-1"
                     />
                     <span>{option.value}</span>
                   </label>
                 ))}
               </li>
-
             ))}
-
           </ul>
+
           <div className='flex flex-col w-auto flex-grow flex-wrap bg-gray-100 dark:bg-gray-900 p-6 rounded-lg border border-gray-200 dark:border-gray-500'>
             <button
               type="submit"
               className="px-4 py-2 bg-blue-600 text-white rounded-md"
             >
-              Generate SKUs
+              Gerar SKUs
             </button>
 
-            <h2 className="text-2xl font-bold mt-8 mb-4 text-black dark:text-white">Generated SKUs</h2>
+            <h2 className="text-2xl font-bold mt-8 mb-4 text-black dark:text-white">SKUs Gerados</h2>
             <table className="w-full table-auto">
               <tbody>
                 {skuList.map((sku, index) => (
